ba8c749d8df61aa91513ff3cf8a6e1ff4a5ac5b1
Marcelo Custodio <smartcontrolbrasiloficial@gmail.com>
2026-05-25 23:07:38 -0300

    refactor: align institutional site with industrial automation

--- apps/institutional/views.py
+++ apps/institutional/views.py
@@ -150,54 +150,6 @@ def project_details(request):
     return render(request, "institutional/eitech/pages/project-details.html")
 
 
-def project_smart360(request):
-    return render(request, "institutional/eitech/pages/projects/smart360.html")
-
-
-def project_diagnostico_ia_dados_automacao(request):
-    return render(request, "institutional/eitech/pages/projects/diagnostico-ia-dados-automacao.html")
-
-
-def project_automacao_industrial_clps(request):
-    return render(request, "institutional/eitech/pages/projects/automacao-industrial-clps.html")
-
-
-def project_manutencao_confiabilidade(request):
-    return render(request, "institutional/eitech/pages/projects/manutencao-confiabilidade.html")
-
-
-def project_cybersecurity_empresas(request):
-    return render(request, "institutional/eitech/pages/projects/cybersecurity-empresas.html")
-
-
-def project_sites_marketing_conteudo(request):
-    return render(request, "institutional/eitech/pages/projects/sites-marketing-conteudo.html")
-
-
-def project_sistemas_web_django_python(request):
-    return render(request, "institutional/eitech/pages/projects/sistemas-web-django-python.html")
-
-
-def project_ia_atendimento_operacao(request):
-    return render(request, "institutional/eitech/pages/projects/ia-atendimento-operacao.html")
-
-
-def project_dashboards_indicadores_operacionais(request):
-    return render(request, "institutional/eitech/pages/projects/dashboards-indicadores-operacionais.html")
-
-
-def project_gestao_os_ativos(request):
-    return render(request, "institutional/eitech/pages/projects/gestao-os-ativos.html")
-
-
-def project_integracao_dados_sistemas(request):
-    return render(request, "institutional/eitech/pages/projects/integracao-dados-sistemas.html")
-
-
-def project_seguranca_backup_continuidade(request):
-    return render(request, "institutional/eitech/pages/projects/seguranca-backup-continuidade.html")
-
-
 def blog(request):
     return render(request, "institutional/eitech/pages/blog.html")
 
@@ -210,37 +162,85 @@ def blog_details(request):
     return render(request, "institutional/eitech/pages/blog-details.html")
 
 
-# Static blog article pages (render templates under pages/blog/)
-def blog_ia_pequenas_empresas(request):
+def supervisao_dados_industriais(request):
+    return render(request, "institutional/eitech/pages/projects/smart360.html")
+
+
+def diagnostico_industrial_engenharia_solucao(request):
+    return render(request, "institutional/eitech/pages/projects/diagnostico-ia-dados-automacao.html")
+
+
+def automacao_industrial_clps_ihms(request):
+    return render(request, "institutional/eitech/pages/projects/automacao-industrial-clps.html")
+
+
+def manutencao_retrofit_confiabilidade(request):
+    return render(request, "institutional/eitech/pages/projects/manutencao-confiabilidade.html")
+
+
+def inversores_eficiencia_energetica(request):
+    return render(request, "institutional/eitech/pages/projects/cybersecurity-empresas.html")
+
+
+def servoacionamentos_motion_control(request):
+    return render(request, "institutional/eitech/pages/projects/sites-marketing-conteudo.html")
+
+
+def supervisorio_scada_dados_industriais(request):
+    return render(request, "institutional/eitech/pages/projects/sistemas-web-django-python.html")
+
+
+def robotica_celulas_automatizadas(request):
+    return render(request, "institutional/eitech/pages/projects/ia-atendimento-operacao.html")
+
+
+def gerenciamento_energia_utilidades(request):
+    return render(request, "institutional/eitech/pages/projects/dashboards-indicadores-operacionais.html")
+
+
+def paineis_eletricos_baixa_tensao(request):
+    return render(request, "institutional/eitech/pages/projects/gestao-os-ativos.html")
+
+
+def integracao_chao_fabrica_dados_industriais(request):
+    return render(request, "institutional/eitech/pages/projects/integracao-dados-sistemas.html")
+
+
+def seguranca_maquinas_continuidade_operacional(request):
+    return render(request, "institutional/eitech/pages/projects/seguranca-backup-continuidade.html")
+
+
+def blog_retrofit_maquina_industrial(request):
     return render(request, "institutional/eitech/pages/blog/ia-pequenas-empresas.html")
 
 
-def blog_organizar_dados_antes_automatizar(request):
+def blog_organizar_sinais_dados_antes_automatizar(request):
     return render(request, "institutional/eitech/pages/blog/organizar-dados-antes-automatizar.html")
 
 
-def blog_manutencao_tpm_confiabilidade_digital(request):
+def blog_manutencao_tpm_confiabilidade_sistemas_automatizados(request):
     return render(request, "institutional/eitech/pages/blog/manutencao-tpm-confiabilidade-digital.html")
 
 
-def blog_cybersecurity_pequenas_empresas(request):
+def blog_clp_ihm_inversor_falhas_maquina(request):
     return render(request, "institutional/eitech/pages/blog/cybersecurity-pequenas-empresas.html")
 
 
-def blog_sistemas_web_operacao_moderna(request):
+def blog_supervisorio_industrial_dados_tempo_real(request):
     return render(request, "institutional/eitech/pages/blog/sistemas-web-operacao-moderna.html")
 
 
-def blog_automacao_industrial_conectada_gestao(request):
+def blog_automacao_industrial_conectada_manutencao_gestao(request):
     return render(request, "institutional/eitech/pages/blog/automacao-industrial-conectada-gestao.html")
 
 
-def blog_marketing_digital_tecnologia_processo(request):
+def blog_paineis_eletricos_organizacao_seguranca_manutenibilidade(request):
     return render(request, "institutional/eitech/pages/blog/marketing-digital-tecnologia-processo.html")
 
 
-def blog_smart360_operacao_inteligente(request):
+def blog_dados_industriais_confiabilidade_reduzir_paradas(request):
     return render(request, "institutional/eitech/pages/blog/smart360-operacao-inteligente.html")
+
 
 def faq(request):
     return render(request, "institutional/eitech/pages/faq.html")
